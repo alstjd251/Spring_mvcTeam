331f655d9052b27393238adb585fe1f0e647a370
Lucky-BE <byungeun96@gmail.com>
2021-11-05 15:31:25 +0900

    메인 페이지 띄우기(아직 수정 필요)

--- src/co/sp/controller/MemControl.java
+++ src/co/sp/controller/MemControl.java
@@ -8,12 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import co.sp.beans.Member_s;
 import co.sp.service.MemService;
 
 @Controller
+@RequestMapping("/member")
 public class MemControl {
 
 	@Autowired
@@ -25,13 +27,13 @@ public class MemControl {
 	@GetMapping("/main")
 	public String main(@ModelAttribute("memberBean") Member_s memberBean, Model m) {
 	
-		return "main";
+		return "member/main";
 	}
 	
 	@PostMapping("/member_proc")
 	public String join(@ModelAttribute("memberBean") Member_s memberBean) {
 		ms.addMember(memberBean);
-		return "page";
+		return "/page";
 	}
 	
 	@GetMapping("/login")

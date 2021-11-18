afdcc8f2786cc02c6d1181e5cd5241f820785f68
alstjd251 <alstjd251@naver.com>
2021-11-18 16:36:29 +0900

    commit

--- WebContent/WEB-INF/views/board/NoticeModifyProc.jsp
+++ WebContent/WEB-INF/views/board/NoticeModifyProc.jsp
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>Sul Sure</title>
 </head>
 <body>
 <script>
--- src/co/sp/interceptor/LoginCheckInterceptor.java
+++ src/co/sp/interceptor/LoginCheckInterceptor.java
@@ -1,6 +1,13 @@
 package co.sp.interceptor;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.AbstractDocument.Content;
+
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import co.sp.beans.Member_s;
+import co.sp.service.NoticeService;
 
 public class LoginCheckInterceptor implements HandlerInterceptor{
 	

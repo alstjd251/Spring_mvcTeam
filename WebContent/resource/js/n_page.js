41f80233a90363f551dde45b223bede49aa012e6
dkxmrhe <dkxmrhe1@daum.net>
2021-11-22 12:30:22 +0900

    main course

--- WebContent/resource/js/n_page.js
+++ WebContent/resource/js/n_page.js
@@ -1,12 +1,12 @@
 $(window).ready( function() {
-	if($("body").height() < $(window).height()) {
+	if(($("header").height() + $("section").height()) < $(window).height()) {
 		$("footer").css({ top :window.innerHeight - 250 + "px"});
 	}else {
 		$("footer").css({top : 0, position : "relative"});
 	}
 });
 $(window).on("resize" , function() {
-	if($("body").height() < $(window).height()) {
+	if(($("header").height() + $("section").height()) < $(window).height()) {
 		$("footer").css({ top :window.innerHeight - 250 + "px"});
 	} else {
 		$("footer").css({top : 0 , position : "relative"});
